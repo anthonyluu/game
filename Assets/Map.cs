e6834e6af084d8eaedb9ad64e6f34a07a99b0f32
Anthony Luu <a6luu@uwaterloo.ca>
2017-01-21 21:54:02 -0500

    first commit made grid. the cell games begin

---
+++ Assets/Map.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+	public int something;
+	public string value;
+	public int width;
+	public int height;
+
+	public GameObject TilePrefab;
+
+	private List<List<Tile>> map;
+
+	// Use this for initialization
+	void Start () {
+		GenerateMap ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void GenerateMap() {
+		map = new List<List<Tile>>();
+		for (int x = 0; x < height; x++) {
+			List<Tile> row = new List<Tile> ();
+			for (int y = 0; y < width; y++) {
+//				Tile tile = new Tile ();
+				Tile tile = ((GameObject)Instantiate( TilePrefab, new Vector3(x - Mathf.Floor(width/2), 0, -y + Mathf.Floor(height/2)), Quaternion.Euler(new Vector3()))).GetComponent<Tile>();
+				row.Add (tile);
+
+			}
+			map.Add (row);
+		}
+	}
+}
