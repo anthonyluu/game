8d0e72c40317f231f780c52261a1f6875a6b2e70
Anthony Luu <a6luu@uwaterloo.ca>
2017-01-22 13:54:52 -0500

    added highlighting on mouse enter and exit. added some transparent material

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -15,8 +15,6 @@ public class Map : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GenerateMap ();
-
-		
 	}
 	
 	// Update is called once per frame
@@ -32,7 +30,6 @@ public class Map : MonoBehaviour {
 			for (int z = 0; z < height; z++) {
 				Tile tile = ((GameObject)Instantiate( TilePrefab, new Vector3(x - Mathf.Floor(width/2), 0, -z + Mathf.Floor(height/2)), Quaternion.Euler(new Vector3()))).GetComponent<Tile>();
 				row.Add (tile);
-
 			}
 			map.Add (row);
 		}
--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -4,16 +4,27 @@ using System.Collections;
 public class Tile : MonoBehaviour
 {
 
+	private Color originalColor;
+
 	// Use this for initialization
 	void Start ()
 	{
-	
+		originalColor = GetComponent<Renderer> ().material.color;
+		
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 	
+	}
+
+	void OnMouseOver() {
+		GetComponent<Renderer> ().material.SetColor ("_Color", Color.yellow);
+	}
+
+	void OnMouseExit() {
+		GetComponent<Renderer> ().material.SetColor ("_Color", originalColor);
 	}
 }
 
