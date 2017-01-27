e4b7c2d044b429fafc07331e2dbbe193adc119e4
Anthony Luu <a6luu@uwaterloo.ca>
2017-01-26 20:12:48 -0500

    moved tile border mat, and added random logic to generate the terrain on the map

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Map : MonoBehaviour {
-	public int something;
-	public string value;
+	[Range(0,1)]
+	public float terrain_ratio = 0.3f; // The lower it is, the less terrain there will be
+
 	public int width;
 	public int height;
 
@@ -15,6 +16,7 @@ public class Map : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GenerateMap ();
+		SetupMapTerrain ();
 	}
 	
 	// Update is called once per frame
@@ -24,6 +26,7 @@ public class Map : MonoBehaviour {
 
 
 	void GenerateMap() {
+		// Initialize the map and add Instantiated Tiles to it
 		map = new List<List<Tile>>();
 		for (int x = 0; x < width; x++) {
 			List<Tile> row = new List<Tile> ();
@@ -32,6 +35,45 @@ public class Map : MonoBehaviour {
 				row.Add (tile);
 			}
 			map.Add (row);
+		}
+	}
+
+	void SetupMapTerrain() {
+		// iterate through map
+		// if its the first or last column, set it to "Home". This is where players will start
+		// else, randomly generate terrain based on terrain_ratio
+
+		for (int x = 0; x < width; x++) {
+			for (int z = 0; z < height; z++) {
+				// We should only have 9 tiles in the x axis. the left and right most are Home TileTypes.
+				if (x == 0 || x == 8) {
+					map [x] [z].tileType = TileType.Home;
+				} else {
+					// get a unif distr to determine which tiles are which
+					map[x][z].tileType = GetRandomTileType();
+
+
+				}
+			}
+		}
+	}
+
+	private TileType GetRandomTileType() {
+		// Randomly generate a value, then compare it with the terrain_ratio
+		// high terrain ratio will result in a lot of terrain
+		// low terrain ratio will result in very little terrain
+
+		// Random value
+		float randomVal = Random.Range (0.0f, 1.0f);
+		Debug.Log (randomVal);
+
+		if (terrain_ratio < randomVal) {
+			Debug.Log ("normal");
+			// we'll keep it normal
+			return TileType.Normal;
+		} else {
+			// we wanna pick a random value out of water, forest, rock, ice
+			return (TileType)Random.Range(0, 3);
 		}
 	}
 }
--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -1,20 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+public enum TileType {
+	Water,
+	Forest,
+	Rock,
+	Ice,
+	Normal,
+	Home,
+}
+
 public class Tile : MonoBehaviour
 {
+	
+	public TileType tileType = TileType.Normal;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		GetComponent<Renderer> ().material.SetColor("_EmissionColor", GetTileColor());
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+
 	}
+
+	Color GetTileColor()
+	{
+		switch (tileType)
+		{
+		case TileType.Water:
+			return Color.blue;
+		case TileType.Forest:
+			return Color.green;
+		case TileType.Rock:
+			return Color.grey;
+		case TileType.Ice:
+			return Color.cyan;
+		case TileType.Normal:
+			return Color.clear;
+		default:
+			// Home color
+			return Color.yellow;
+		};
+
+	}
+
 
 	void OnMouseOver() {
 		GetComponent<Renderer> ().material.SetColor("_EmissionColor", Color.white);
@@ -22,7 +56,7 @@ public class Tile : MonoBehaviour
 	}
 
 	void OnMouseExit() {
-		GetComponent<Renderer> ().material.SetColor("_EmissionColor", Color.black);
+		GetComponent<Renderer> ().material.SetColor("_EmissionColor", GetTileColor());
 	}
 }
 
